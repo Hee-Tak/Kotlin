25f53dcfd509a6ec79e4e1836ee85edc392e3821
hitak77 <hitak77@naver.com>
2023-02-05 21:03:18 +0900

    list test

---
+++ ListTest/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
+    val numbers1 = listOf(1, 2, 3, 4, 5, 6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+
+}

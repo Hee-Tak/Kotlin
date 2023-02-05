5b4696a898fe37bc57543699ee861562a08a904f
hitak77 <hitak77@naver.com>
2023-02-06 03:34:25 +0900

    2023 02 06 / AM 03 34 studying about List

--- ListTest/src/main/kotlin/Main.kt
+++ ListTest/src/main/kotlin/Main.kt
@@ -1,9 +1,18 @@
 fun main(args: Array<String>) {
     val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
     val numbers1 = listOf(1, 2, 3, 4, 5, 6)
+
     println("List: $numbers")
     println("Size: ${numbers.size}")
+
+    // Access elements of the list
     println("First element: ${numbers[0]}")
     println("Second element: ${numbers[1]}")
+    println("Last index: ${numbers.size -1}")
+    println("Last element: ${numbers[numbers.size-1]}")
+
+    // Use the contains() method
+    println("Contains 4? ${numbers.contains(4)}")
+    println("Contains 7? ${numbers.contains(7)}")
 
 }

43c18e70ab992897ea497ba66f219af81bc3532f
hitak77 <hitak77@naver.com>
2023-04-12 06:55:46 +0900

    2023 04 12 map

--- Collection_map/src/main/kotlin/map.kt
+++ Collection_map/src/main/kotlin/map.kt
@@ -8,4 +8,15 @@ fun main() {
     peopleAges["Joe"] = 51
     peopleAges["Fred"] = 31
     println(peopleAges)
+
+    /*
+    peopleAges.forEach{print("${it.key} is ${it.value}, ")}
+    //자동으로 모든 항목을 탐색한 후 항목별로 작업을 실행하는 forEach
+    // for와 비슷하지만 조금 더 컴팩트함.
+    */
+    println(peopleAges.map{"${it.key} is ${it.value}"}.joinToString(", "))
+
+    val filterNames = peopleAges.filter{it.key.length < 4}
+    println(filterNames)
+
 }
